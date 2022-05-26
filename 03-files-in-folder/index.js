6a8a405155daabeefa450ab7893bf1d080ee988e
Glav-Tz <GlavTz@gmail.com>
2022-05-26 18:28:27 +0300

    add 4 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,37 +1,9 @@
 const fs = require ('fs');
 const path = require('path');
-
 const pathFolder = path.join(__dirname, 'secret-folder');
 
-let tempArr = [];
-async function fileSize(element) {
-  const promiseFileSize = new Promise((resolve, reject) => {
-    let currentFile = path.join(pathFolder, element.name);
-    fs.stat(currentFile, (err, stats) => {
-      if (err) {
-        return reject('Упс!_fileSize');
-      }else {        
-        return resolve(stats.size);
-      }                  
-    });        
-  });
-  return promiseFileSize;
-}
-
-
-async function arrInfoFile (fileSize, file) {
-  const promiseInfo = new Promise((resolve) => {
-    let arrInfoOneFile = [];  
-    let extEnd = (file.split('.')).pop();
-    let extFirst = (file.split('.')).shift();
-    arrInfoOneFile.push(extFirst, extEnd, fileSize);
-    return resolve(arrInfoOneFile.join('-'));  
-  });
-  return promiseInfo;
-}
-
 async function listFiles(pathFolder) {
-  const promiseReaddir = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.readdir(pathFolder, { withFileTypes: true }, (err, files) => {
       if (err) {
         return reject('Упс!_listFiles');
@@ -39,35 +11,31 @@ async function listFiles(pathFolder) {
         return resolve(files);
       }    
     });
-  });
-  return promiseReaddir;
+  });  
 }
 
-async function infoFiles(arrData) {
-  const promiseInfoFile = new Promise((resolve) => {
-    arrData.forEach((element) => {
-      if (element.isDirectory()) {
-        console.log(1);        
-      }else {
-        fileSize(element)
-          .then(size => arrInfoFile(size, element.name))
-          .then(stringInfo => {
-            console.log(stringInfo);
-            return tempArr.push(stringInfo);
-          });        
-      }
-    });    
-    resolve(tempArr);
+function infoFiles(list) {  
+  list.forEach((element) => {
+    if (!element.isDirectory()) {
+      let arrInfoOneFile = [];
+      let currentFileFs = path.join(pathFolder, element.name);
+      let extEnd = (element.name.split('.')).pop();
+      let extFirst = (element.name.split('.')).shift();      
+      fs.stat(currentFileFs, (err, stats) => {          
+        if (err) {
+          return (console.log('Упс!_fileSize'));
+        } else {            
+          arrInfoOneFile.push(extFirst, extEnd, stats.size);            
+          console.log(arrInfoOneFile.join('-'));
+        }                  
+      });
+    }  
   });
-  return promiseInfoFile;
 }
 
 async function asyncStart() {
-  let step1 = await listFiles(pathFolder);
-  let step2 = await infoFiles(step1);
-  console.log('step2');
-  console.log(step2);  
+  let list = await listFiles(pathFolder);
+  infoFiles(list);  
 }
 
 asyncStart();
-
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,73 @@
+const fs = require ('fs');
+const path = require('path');
+const copyDirectory = path.join(__dirname, 'files-copy');
+const rootDirectory = path.join(__dirname, 'files');
+
+async function creatingDirectory(pathFolder) {  
+  fs.mkdir(pathFolder, err => {
+    if(err) {
+      console.log('Папку уже существуюет, запускаю актуализацию файлов.');
+      return (updatingFile());
+    }
+    console.log('Создаю папку, запускаю копирование файлов.');
+    return copy();   
+  });
+}
+
+async function listFiles(pathFolder) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(pathFolder, (err, files) => {
+      if (err) {
+        return reject('Упс!_listFiles');
+      } else {       
+        return resolve(files);
+      }    
+    });
+  });  
+}
+
+async function fileCopy(nameFileArr) {  
+  return new Promise((resolve, reject) => {    
+    nameFileArr.forEach((element) => {
+      let whereFrom = path.join(rootDirectory, element);
+      let whereCopy = path.join(copyDirectory, element);      
+      fs.copyFile(whereFrom, whereCopy, err => {
+        if(err) {
+          return reject((console.log('Упс!_nameFileArr'))); 
+        }        
+      });      
+    });
+    resolve ();
+  });
+}
+
+async function DelFile(nameFileArr) {
+  return new Promise((resolve, reject) => {    
+    nameFileArr.forEach((element) => {
+      let whereDelete = path.join(copyDirectory, element);      
+      fs.unlink(whereDelete, err => {
+        if(err) {
+          return reject((console.log('Упс!_Del'))); 
+        }       
+      });
+    });
+    return resolve();
+  });
+}
+
+async function copy() {  
+  const nameFileArr = await listFiles(rootDirectory);    
+  await fileCopy(nameFileArr);
+  console.log('Файлы успешно скопированы.');  
+}
+
+async function updatingFile() {
+  const nameFileArr = await listFiles(copyDirectory);
+  await DelFile(nameFileArr);
+  await fileCopy(nameFileArr);
+  console.log('Актуализацию закончена.'); 
+}
+
+creatingDirectory(copyDirectory);
+
 
